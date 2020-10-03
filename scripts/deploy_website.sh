ad1ca4c390e856be2315b780e7f241cae9da6ad6
Lulu <vannary251@gmail.com>
2020-10-02 21:51:30 -0400

    updated website deploy script

--- scripts/deploy_website.sh
+++ scripts/deploy_website.sh
@@ -4,11 +4,11 @@ set -e
 
 cd $(git rev-parse --show-toplevel)
 
+git reset .
 rm -rf build/*
 npm run build
 rm -rf website/*
 cp -r build/* website/
-git reset .
 git add website
 git commit -m "updated website"
 git push
