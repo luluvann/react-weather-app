b9a16b9fb044f61a97b139cde6ee0b9c57a83f38
Lulu <vannary251@gmail.com>
2020-10-03 22:08:17 -0400

    add functionality to bookmark url per city searched with params

--- src/App.js
+++ src/App.js
@@ -12,13 +12,30 @@ class App extends React.Component {
     this.state = { weatherData: undefined};
     this.startSearch = this.startSearch.bind(this);
     this.getWeather = this.getWeather.bind(this);
-    this.startSearch("Montreal")
+
+    //The 2 below variables work together and will read the value specified to the parameter "city" of the browser url https://myapp.com/?city={value_specified_to_city_param}
+    let params = (new URL(document.location)).searchParams;
+    let city = params.get('city');
+
+    //At the launch of the app, Montreal will be the argument to the function startSearch if no value is specified to the params city of the url, if specified, it will trigger the search of for the city specified
+    if (city == null){
+      this.startSearch("Montreal");
+    } else {
+      this.startSearch(city);
+    }
   }
 
   startSearch(cityName) {
+    //let queryParams = new URLSearchParams(window.location.search)
+    let params = (new URL(document.location)).searchParams
+    // Set new or modify existing parameter value
+    params.set("city", cityName);
+    // Replace current querystring with the new one
+    window.history.replaceState(null, null, "?"+params.toString());
     this.getWeather(cityName, "metric");
   }
 
+  //getWeather is the function that calls the API from the OWM
   getWeather(city, units) {
     if (city == null || units == null) {
       return;
